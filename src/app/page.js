f4c1b612fa4a6683513a465e5596c8fedccda2e8
Faiyyaz25003 <khanwork25003@gmail.com>
2025-10-05 10:44:12 +0530

    feat: add Navbar and Sidebar components with responsive design and user interactions

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 
 
+import Navbar from '@/Components/Layout/Navbar/Navbar'
+import Sidebar from '@/Components/Layout/Sidebar/Sidebar'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-     
+      <Sidebar />
+      <Navbar/>
     </div>
   )
 }
